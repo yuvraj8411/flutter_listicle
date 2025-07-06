d0646f45af4030d49ac1647201ded14182f1eff5
Yuvraj Baravkar <yuvrajbaravkar84@gmail.com>
2025-07-06 11:48:56 +0530

    #Implementation of Sign Up Screen work in progrss

---
+++ lib/screen/sign_up_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:listicle/utilities/constant.dart';
+import 'package:listicle/widget/text_field.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  icon: Image.asset(
+                    'assets/auth_back.png',
+                    width: 20,
+                    height: 20,
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                const SizedBox(width: 8),
+                const Text('Sign Up', style: AppTextStyles.appBarTitle),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTextField(
+                    label: 'Name',
+                    hintText: 'Yuvraj Baravkar',
+                  ),
+                  SizedBox(height: 20),
+                  CustomTextField(
+                    label: 'E-mail',
+                    hintText: 'yuvrajbaravkar84@gmail.com',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 20),
+                  CustomTextField(
+                    label: 'Password',
+                    hintText: '*********',
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
